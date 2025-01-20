7a45753c83217c872e43af231f5e5d35be3af4d9
Brandon Duke <brandon@bduke.dev>
2025-01-20 08:38:56 -0500

    add components

---
+++ src/app/components/atoms/qr-reader/qr-reader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-reader',
+  standalone: true,
+  imports: [],
+  templateUrl: './qr-reader.component.html',
+  styleUrl: './qr-reader.component.scss'
+})
+export class QrReaderComponent {
+
+}
